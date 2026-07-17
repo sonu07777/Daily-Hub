0e58d3b156f07d4c8ca345a50ca7460b495d13bc
sonu sahoo <sonusahoo510@gmail.com>
2026-07-17 22:12:28 +0530

    question(med #189 leet)

---
+++ src/rotateArray.java
@@ -0,0 +1,28 @@
+public class rotateArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        rotate(nums, 3);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+
+}
